34398f809e86540733e476ad27e748d4a1231a5e
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 21:00:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {'The Heavy': 'How Do You Like Me Now?'}
 
 
 function updatePlaylist(playlist, artist, song) {
-  playlist['artist'] = song
+  playlist[artist] = song
   return playlist
 }
