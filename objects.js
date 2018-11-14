b755bc4e0e0f009b635307d73d4d5bb780a1fc58
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 20:49:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ function updateObjectWithKeyAndValue (obj, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
 
+const nuts = {peanuts: 10}
+
+updateObjectWithKeyAndValue(nuts, cashews, 15)
+
+console.log(nuts)
